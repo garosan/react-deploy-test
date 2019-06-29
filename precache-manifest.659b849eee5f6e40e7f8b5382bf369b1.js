d3f56ef5d5a0e2a893b7ebca66bcc4f7620ef8ef
Garo San <garosanchez27@gmail.com>
2019-06-29 18:58:33 -0500

    Updates

---
+++ precache-manifest.659b849eee5f6e40e7f8b5382bf369b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8467dda9fa42a83d691456be0ba85c",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "adeddb410361414927b6",
+    "url": "/react-tutorial/static/css/main.251767c1.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "adeddb410361414927b6",
+    "url": "/react-tutorial/static/js/main.9e45f046.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
